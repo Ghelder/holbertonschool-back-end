ebbe69bb53b1689ac021beb88c37b85c1e808e55
Ghelder <Jhelder@outlook.com>
2023-08-28 22:51:14 -0500

    fix: runs in main

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """export data in the JSON format"""
-from requests import get
 from json import dump
+from requests import get
 
 
-def export_tasks_to_json():
-    url = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    URL = "https://jsonplaceholder.typicode.com"
 
-    users_response = get(f"{url}/users")
-    todos_response = get(f"{url}/todos")
-
-    if users_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Unable to retrieve data from the API")
-        return
+    users_response = get(f"{URL}/users")
+    todos_response = get(f"{URL}/todos")
 
     users = users_response.json()
     todos = todos_response.json()
@@ -20,12 +16,12 @@ def export_tasks_to_json():
     data = {}
 
     for user in users:
-        user_id = str(user["id"])
+        USER_ID = str(user["id"])
         username = user["username"]
         tasks = []
 
         for todo in todos:
-            if todo["userId"] == int(user_id):
+            if todo["userId"] == int(USER_ID):
                 task = {
                     "username": username,
                     "task": todo["title"],
@@ -33,13 +29,7 @@ def export_tasks_to_json():
                 }
                 tasks.append(task)
 
-        data[user_id] = tasks
+        data[USER_ID] = tasks
 
     with open("todo_all_employees.json", "w", encoding="utf-8") as f:
         dump(data, f)
-
-    print("Data exported to todo_all_employees.json successfully.")
-
-
-if __name__ == "__main__":
-    export_tasks_to_json()
