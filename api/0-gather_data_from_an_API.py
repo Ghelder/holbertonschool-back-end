304d3179d01fc7cd587b4377aa7010d4892d5d4c
Ghelder <Jheldernq@outlook.com>
2024-01-27 18:44:05 +0000

    feat: returns information

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""using the API"""
+from sys import argv
+from requests import get
+
+if __name__ == "__main__":
+    URL = "https://jsonplaceholder.typicode.com"
+
+    all_users = get(url=f"{URL}/todos", params={"userId": argv[1]}).json()
+    users = get(url=f"{URL}/users/{argv[1]}").json()
+    name = users.get("name")
+
+    num_tasks = [i for i in all_users if i.get("completed")]
+
+    all_tasks = [i.get("completed") for i in all_users]
+    print(f"Employee {name} is done with tasks({len(num_tasks)}/"
+          f"{len(all_tasks)}):")
+
+    for i in all_users:
+        if i.get("completed"):
+            print(f"\t {i.get('title')}")
